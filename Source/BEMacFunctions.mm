5f140f916d40592a10bc0f2a2d45493e4876ca2f
Mark Banks <minstral@me.com>
2011-07-04 08:36:23 +1000

    Objective-C categories & FileMaker plug-ins are not a happy combination

--- Source/BEMacFunctions.mm
+++ Source/BEMacFunctions.mm
@@ -9,11 +9,66 @@
  
  */
 
-#import "BENSString.h"
 #import "BEMacFunctions.h"
+#import "BEPluginGlobalDefines.h"
+#import "BEPluginUtilities.h"
+
+#import <Cocoa/Cocoa.h>
+
+#if TARGET_RT_BIG_ENDIAN
+#define ENCODING kCFStringEncodingUTF32BE
+#else
+#define ENCODING kCFStringEncodingUTF32LE
+#endif
+
+const NSStringEncoding kEncoding_wchar_t = CFStringConvertEncodingToNSStringEncoding ( ENCODING );
+
+
+NSString * NSStringFromStringAutoPtr ( StringAutoPtr text );
+NSString * NSStringFromWStringAutoPtr ( const WStringAutoPtr text );
+WStringAutoPtr WStringAutoPtrFromNSString ( const NSString * text );
 
 
 WStringAutoPtr SelectFileOrFolder ( WStringAutoPtr prompt, bool choose_file );
+
+
+#pragma mark -
+#pragma mark String Utilities
+#pragma mark -
+
+NSString * NSStringFromStringAutoPtr ( StringAutoPtr text )
+{
+	NSString * new_string = [NSString stringWithCString: text->c_str() encoding: NSUTF8StringEncoding];
+	
+	return new_string;
+}
+
+
+/*
+ NSStringFromWStringAutoPtr & WStringAutoPtrFromNSString from code at
+ 
+ http://www.cocoabuilder.com/archive/cocoa/200434-nsstring-from-wstring.html
+ */
+
+NSString * NSStringFromWStringAutoPtr ( const WStringAutoPtr text )
+{
+	char * string_data = (char *)text->data();
+	unsigned size = text->size() * sizeof ( wchar_t );
+	
+	NSString* new_string = [[[NSString alloc] initWithBytes: string_data
+													 length: size
+												   encoding: kEncoding_wchar_t] autorelease];
+	return new_string;
+}
+
+
+WStringAutoPtr WStringAutoPtrFromNSString ( const NSString * text )
+{
+	NSData * string_data = [text dataUsingEncoding: kEncoding_wchar_t];
+	unsigned size = [string_data length] / sizeof ( wchar_t );
+	
+	return WStringAutoPtr ( new wstring ( (wchar_t *)[string_data bytes], size ) );
+}
 
 
 #pragma mark -
