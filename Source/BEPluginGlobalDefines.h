303f4243132f30e5342c04f44eb1cf6a6aff8e7c
Mark Banks <mark@banks.id.au>
2012-01-31 11:20:57 +1100

    Version 1.3

--- Source/BEPluginGlobalDefines.h
+++ Source/BEPluginGlobalDefines.h
@@ -44,9 +44,9 @@ extern fmx::errcode g_last_error;
 #define PLUGIN_NAME		"BaseElements"
 #define PLUGIN_ID		'G', 'y', 'B', 'E'
 
-#define	VERSION_NUMBER_STRING L"1.2.57"
-#define	AUTO_UPDATE_VERSION L"00010257"
-#define VERSION_STRING "1.3b1"
+#define	VERSION_NUMBER_STRING L"1.3.0"
+#define	AUTO_UPDATE_VERSION L"00010300"
+#define VERSION_STRING "1.3"
 
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/"VERSION_STRING
