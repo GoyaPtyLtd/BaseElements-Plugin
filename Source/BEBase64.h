625d27b78e84eb198604dff9e4f19347dbf93a82
Mark Banks <mark@banks.id.au>
2014-03-20 16:25:13 +1100

    Refactor base64 functions

---
+++ Source/BEBase64.h
@@ -0,0 +1,28 @@
+/*
+ BEBase64.h
+ BaseElements Plug-In
+ 
+ Copyright 2014 Goya. All rights reserved.
+ For conditions of distribution and use please see the copyright notice in BEPlugin.cpp
+ 
+ http://www.goya.com.au/baseelements/plugin
+ 
+ */
+
+
+#ifndef __BaseElements__BEBase64__
+#define __BaseElements__BEBase64__
+
+
+#include "BEPluginGlobalDefines.h"
+#include "BEPluginUtilities.h"
+
+
+#include <vector>
+
+
+std::vector<unsigned char> Base64_Decode ( StringAutoPtr text );
+StringAutoPtr Base64_Encode ( std::vector<unsigned char> data, bool base64url = false );
+
+
+#endif /* defined(__BaseElements__BEBase64__) */
