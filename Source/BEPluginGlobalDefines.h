65ff6cce751e6c39397059b2c960256d0650b5e2
Mark Banks <mark@banks.id.au>
2014-04-02 15:50:33 +1100

    Auto update version 02020153

--- Source/BEPluginGlobalDefines.h
+++ Source/BEPluginGlobalDefines.h
@@ -63,6 +63,7 @@
 
 #include "BECppUtilities.h"
 
+
 extern fmx::errcode g_last_error;
 
 
@@ -73,7 +74,7 @@ extern fmx::errcode g_last_error;
 #define PLUGIN_ID		'G', 'y', 'B', 'E'
 
 #define	VERSION_NUMBER_STRING L"2.3.0"
-#define	AUTO_UPDATE_VERSION L"02020152"
+#define	AUTO_UPDATE_VERSION L"02020153"
 #define VERSION_STRING "2.3"
 
 
