4f15b1a899add2dc82ae87d8aea8bf68a1ce2532
Mark Banks <mark@banks.id.au>
2014-04-11 20:00:09 +1000

    Version 2.2.2 Update OpenSSL to 1.0.1g (Heartbleed bug) Update Curl to 7.36

--- Source/BEPluginGlobalDefines.h
+++ Source/BEPluginGlobalDefines.h
@@ -72,9 +72,9 @@ extern fmx::errcode g_last_error;
 #define PLUGIN_NAME		"BaseElements"
 #define PLUGIN_ID		'G', 'y', 'B', 'E'
 
-#define	VERSION_NUMBER_STRING L"2.2.1"
-#define	AUTO_UPDATE_VERSION L"02020100"
-#define VERSION_STRING "2.2.1"
+#define	VERSION_NUMBER_STRING L"2.2.2"
+#define	AUTO_UPDATE_VERSION L"02020200"
+#define VERSION_STRING "2.2.2"
 
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/"VERSION_STRING
