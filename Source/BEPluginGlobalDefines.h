77a4f6c3ea8a7f1e163ee81886e41f8e3137850c
Mark Banks <mark@banks.id.au>
2014-05-06 17:29:54 +1000

    Auto Update Version 02020157

--- Source/BEPluginGlobalDefines.h
+++ Source/BEPluginGlobalDefines.h
@@ -74,7 +74,7 @@ extern fmx::errcode g_last_error;
 #define PLUGIN_ID		'G', 'y', 'B', 'E'
 
 #define	VERSION_NUMBER_STRING L"2.3.0"
-#define	AUTO_UPDATE_VERSION L"02020156"
+#define	AUTO_UPDATE_VERSION L"02020157"
 #define VERSION_STRING "2.3a2"
 
 
