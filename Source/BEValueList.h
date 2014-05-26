c7e8c6621b74037173243d95166c743ed3c20e61
Mark Banks <mark@banks.id.au>
2014-05-27 09:30:50 +1000

    Change to template class to allow value lists of both string and wstring

--- Source/BEValueList.h
+++ Source/BEValueList.h
@@ -16,7 +16,17 @@
 
 #include <string>
 #include <vector>
+#include <sstream>
+#include <set>
+#include <algorithm>
 
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/classification.hpp>
+
+
+
+template <typename T>
 
 class BEValueList {
 	
@@ -24,30 +34,189 @@ class BEValueList {
 public:
 	
 	BEValueList ( void );
-	BEValueList ( const std::string value_list );
+	BEValueList ( const T value_list );
+	BEValueList ( const std::vector<T> value_list );
 	
 	bool not_empty ( );
 	size_t size ( ) const;
 
 	void append ( BEValueList to_append );
-	void append ( const std::string to_append );
+	void append ( const T to_append );
 	
-	std::string first ( ) const;
-	std::string at ( const size_t which ) const;
+	T first ( ) const;
+	T at ( const size_t which ) const;
 	
-	std::string unique ( );
-	std::string filter_out ( std::auto_ptr<BEValueList> filter_out );
-	std::string sort ( );
+	T unique ( );
+	T filter_out ( std::auto_ptr<BEValueList> filter_out );
+	T sort ( );
 	
-	std::vector<std::string> get_values ( void ) { return values; }
-	std::string get_as_comma_separated ( void );
-	std::string get_as_filemaker_string ( void );
+	std::vector<T> get_values ( void ) { return values; }
+	T get_as_comma_separated ( void );
+	T get_as_filemaker_string ( void );
 	
 protected:
 	
-	std::vector<std::string> values;
+	std::vector<T> values;
 	
 };
+
+
+
+
+template <typename T>
+BEValueList<T>::BEValueList ( void )
+{
+	values.clear();
+}
+
+
+template <typename T>
+BEValueList<T>::BEValueList ( const T value_list )
+{
+	boost::split ( values, value_list, boost::is_any_of ( FILEMAKER_END_OF_LINE ), boost::token_compress_on );
+}
+
+
+template <typename T>
+BEValueList<T>::BEValueList ( const std::vector<T> value_list )
+{
+	values = value_list;
+}
+
+
+template <typename T>
+bool BEValueList<T>::not_empty ( )
+{
+	return !values.empty();
+}
+
+
+template <typename T>
+size_t BEValueList<T>::size ( ) const
+{
+	return values.size();
+}
+
+
+template <typename T>
+void BEValueList<T>::append ( BEValueList<T> to_append )
+{
+	const std::vector<T> new_values = to_append.get_values();
+	values.insert ( values.end(), new_values.begin(), new_values.end() );
+}
+
+
+template <typename T>
+void BEValueList<T>::append ( const T to_append )
+{
+	values.push_back ( to_append );
+}
+
+
+template <typename T>
+T BEValueList<T>::first ( ) const
+{
+	return values.front();
+};
+
+
+template <typename T>
+T BEValueList<T>::at ( const size_t which ) const
+{
+	return values.at ( which );
+}
+
+
+template <typename T>
+T BEValueList<T>::unique ( )
+{
+	
+	std::set<T> unique;
+	std::stringstream text;
+	
+	for ( typename std::vector<T>::iterator it = values.begin() ; it != values.end(); ++it ) {
+		
+		std::pair<typename std::set<T>::iterator, bool> inserted = unique.insert ( *it );
+		
+		if ( inserted.second == true ) {
+			
+			if ( text.rdbuf()->in_avail() > 0 ) {
+				text << FILEMAKER_END_OF_LINE;
+			}
+			text << *it;
+		}
+		
+	}
+	
+	return text.str();
+	
+} // unique
+
+
+template <typename T>
+T BEValueList<T>::filter_out ( std::auto_ptr<BEValueList> filter_out )
+{
+	
+	const std::vector<T> to_filter = filter_out->get_values();
+	std::set<T> filter_set ( to_filter.begin(), to_filter.end() );
+	
+	std::stringstream text;
+	
+	for ( typename std::vector<T>::iterator it = values.begin() ; it != values.end(); ++it ) {
+		
+		std::pair<typename std::set<T>::iterator, bool> inserted = filter_set.insert ( *it );
+		
+		if ( inserted.second == true ) {
+			
+			if ( text.rdbuf()->in_avail() > 0 ) {
+				text << FILEMAKER_END_OF_LINE;
+			}
+			text << *it;
+			filter_set.erase ( *it );
+		}
+		
+	}
+	
+	return text.str();
+	
+} // filter_out
+
+
+template <typename T>
+T BEValueList<T>::sort ( ) {
+	
+	std::vector<T> sorted ( values.begin(), values.end() );
+	std::sort ( sorted.begin(), sorted.end() );
+	
+	std::stringstream text;
+	
+	for ( typename std::vector<T>::iterator it = sorted.begin() ; it != sorted.end(); ++it ) {
+		
+		if ( text.rdbuf()->in_avail() > 0 ) {
+			text << FILEMAKER_END_OF_LINE;
+		}
+		text << *it;
+		
+	}
+	
+	return text.str();
+	
+} // sort
+
+
+template <typename T>
+T BEValueList<T>::get_as_comma_separated ( void )
+{
+	return boost::algorithm::join ( values, "," );
+}
+
+
+template <typename T>
+T BEValueList<T>::get_as_filemaker_string ( void )
+{
+	return boost::algorithm::join ( values, FILEMAKER_END_OF_LINE );
+}
+
 
 
 #endif /* defined(__BaseElements__BEValueList__) */
