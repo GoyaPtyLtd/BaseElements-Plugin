6c20561368fd8bd6999e3fe3953e7e17f3b75c40
Mark Banks <mark@banks.id.au>
2015-01-13 08:58:33 +1100

    Bump version numbers and strings for 3.1a1

--- Source/BEPluginGlobalDefines.h
+++ Source/BEPluginGlobalDefines.h
@@ -77,9 +77,9 @@ extern fmx::errcode g_last_error;
 #define PLUGIN_ID		'G', 'y', 'B', 'E'
 #define BUNDLE_STRINGS_ID "BaseElements"
 
-#define	VERSION_NUMBER_STRING L"3.0.0"
-#define	AUTO_UPDATE_VERSION L"03000000"
-#define VERSION_STRING "3.0"
+#define	VERSION_NUMBER_STRING L"3.1.0"
+#define	AUTO_UPDATE_VERSION L"03009000"
+#define VERSION_STRING "3.1"
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/"VERSION_STRING
 
