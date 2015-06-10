ba8fef02434b3b1d82252900adbf09be7ec8d354
Mark Banks <mark@banks.id.au>
2015-06-11 09:41:40 +1000

    Update copyright notice

--- Source/BEMessageDigest.cpp
+++ Source/BEMessageDigest.cpp
@@ -2,7 +2,7 @@
  BEMessageDigest.cpp
  BaseElements Plug-In
  
- Copyright 2011-2014 Goya. All rights reserved.
+ Copyright 2011-2015 Goya. All rights reserved.
  For conditions of distribution and use please see the copyright notice in BEPlugin.cpp
  
  http://www.goya.com.au/baseelements/plugin
