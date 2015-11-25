ba03c18f4773e0b9cc52324268a996689c9b5973
Mark Banks <mark@banks.id.au>
2015-11-25 14:09:50 +1100

    Correct format of version string

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -17,7 +17,7 @@
 #define BUNDLE_VERSION_ID "BEPluginVersion"
 #define PLUGIN_DESCRIPTION_STRING_ID 65000
 
-#define	VERSION_NUMBER_STRING L"3.2"
+#define	VERSION_NUMBER_STRING L"3.2.0"
 #define	AUTO_UPDATE_VERSION L"03019900"
 #define VERSION_STRING "3.2a1" // also in xcode project build settings
 
