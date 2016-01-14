10e3f1b5f229ea56502f7bd8132577bd163fb8cf
Mark Banks <mark@banks.id.au>
2016-01-14 16:56:09 +1100

    Version 3.2

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -2,7 +2,7 @@
  BEPluginVersion.h
  BaseElements Plug-In
  
- Copyright 2015 Goya. All rights reserved.
+ Copyright 2015-2016 Goya. All rights reserved.
  For conditions of distribution and use please see the copyright notice in BEPlugin.cpp
  
  http://www.goya.com.au/baseelements/plugin
@@ -18,8 +18,8 @@
 #define PLUGIN_DESCRIPTION_STRING_ID 65000
 
 #define	VERSION_NUMBER_STRING L"3.2.0"
-#define	AUTO_UPDATE_VERSION L"03019903"
-#define VERSION_STRING "3.2a1" // also in xcode project build settings
+#define	AUTO_UPDATE_VERSION L"03020000"
+#define VERSION_STRING "3.2" // also in xcode project build settings
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
 
