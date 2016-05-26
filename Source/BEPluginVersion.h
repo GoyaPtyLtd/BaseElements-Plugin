03c3f1ea23a91a81e93985185b203f6f9e5d1dda
Mark Banks <mark@banks.id.au>
2016-05-26 13:18:51 +1000

    Build/AutoUpdateVersion 03029909

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -18,7 +18,7 @@
 #define PLUGIN_DESCRIPTION_STRING_ID 65000
 
 #define	VERSION_NUMBER_STRING L"3.3.0"
-#define	AUTO_UPDATE_VERSION L"03029908"
+#define	AUTO_UPDATE_VERSION L"03029909"
 #define VERSION_STRING "3.3b1" // also in xcode project build settings
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
