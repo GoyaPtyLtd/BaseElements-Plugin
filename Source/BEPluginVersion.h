2cb1cbeef0c64329e60de5f6e0af4cbd1d76e294
Mark Banks <mark@Abu.local>
2016-12-09 11:11:02 +1100

    Version 3.3.3

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -17,9 +17,9 @@
 #define BUNDLE_VERSION_ID "BEPluginVersion"
 #define PLUGIN_DESCRIPTION_STRING_ID 65000
 
-#define	VERSION_NUMBER_STRING L"3.3.2"
-#define	AUTO_UPDATE_VERSION L"03030200"
-#define VERSION_STRING "3.3.2" // also in xcode project build settings
+#define	VERSION_NUMBER_STRING L"3.3.3"
+#define	AUTO_UPDATE_VERSION L"03030300"
+#define VERSION_STRING "3.3.3" // also in xcode project build settings
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
 
