ca0f92aec4a147078e838c10c64c825ed5a7e300
Mark Banks <mark@goya.local>
2017-07-21 16:36:00 +1000

    Release 4.0b3

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -17,9 +17,9 @@
 #define BUNDLE_VERSION_ID "BEPluginVersion"
 #define PLUGIN_DESCRIPTION_STRING_ID 65000
 
-#define	VERSION_NUMBER_STRING L"3.99.5"
-#define	AUTO_UPDATE_VERSION L"03990005"
-#define VERSION_STRING "4.0b2" // also in xcode project build settings
+#define	VERSION_NUMBER_STRING L"3.99.6"
+#define	AUTO_UPDATE_VERSION L"03990006"
+#define VERSION_STRING "4.0b3" // also in xcode project build settings
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
 
