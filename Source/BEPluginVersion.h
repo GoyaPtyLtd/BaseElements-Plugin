e91979199a9df2839e29e96865e81a9542273610
Mark Banks <mark@goya.local>
2017-07-25 13:54:27 +1000

    Allow a file (@/path/...) to be specified as the post args for a POST

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -17,8 +17,8 @@
 #define BUNDLE_VERSION_ID "BEPluginVersion"
 #define PLUGIN_DESCRIPTION_STRING_ID 65000
 
-#define	VERSION_NUMBER_STRING L"3.99.7"
-#define	AUTO_UPDATE_VERSION L"03990007"
+#define	VERSION_NUMBER_STRING L"3.99.8"
+#define	AUTO_UPDATE_VERSION L"03990008"
 #define VERSION_STRING "4.0b3" // also in xcode project build settings
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
