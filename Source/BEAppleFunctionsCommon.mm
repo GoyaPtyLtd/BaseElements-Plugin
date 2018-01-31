3234a3dfc6c2e49427737c589872fcaac75799ba
Mark Banks <mark@abu.local>
2018-01-31 13:05:39 +0100

    Add guards for CFRelease

--- Source/BEAppleFunctionsCommon.mm
+++ Source/BEAppleFunctionsCommon.mm
@@ -24,23 +24,23 @@ const unsigned long Sub_OSXLoadString ( unsigned long stringID, FMX_Unichar* int
 	
 	if( (intoHere != NULL) && (intoHereMax > 1) ) {
 		// Turn stringID to a textual identifier, then get the string from the .strings file as a null-term unichar array.
-		CFStringRef 	strIdStr = CFStringCreateWithFormat( kCFAllocatorDefault, NULL, CFSTR("%ld"), stringID );
-		CFStringRef 	bundleIdStr = CFStringCreateWithCString ( NULL, bundleId.c_str(), (unsigned int)bundleId.size() );
+		CFStringRef strIdStr = CFStringCreateWithFormat ( kCFAllocatorDefault, NULL, CFSTR("%ld"), stringID );
+		CFStringRef bundleIdStr = CFStringCreateWithCString ( NULL, bundleId.c_str(), (unsigned int)bundleId.size() );
 		
 		// Note: The plug-in must be explicit about the bundle and file it wants to pull the string from.
-		CFStringRef 	osxStr = CFBundleCopyLocalizedString ( reinterpret_cast<CFBundleRef>(gFMX_ExternCallPtr->instanceID), strIdStr, strIdStr, bundleIdStr );
+		CFStringRef osxStr = CFBundleCopyLocalizedString ( reinterpret_cast<CFBundleRef>(gFMX_ExternCallPtr->instanceID), strIdStr, strIdStr, bundleIdStr );
 		
 		if((osxStr != NULL) && (osxStr != strIdStr)) {
 			
-			long	osxStrLen = CFStringGetLength(osxStr);
+			long osxStrLen = CFStringGetLength ( osxStr );
 			
-			if( osxStrLen < (intoHereMax-1) ) {
+			if ( osxStrLen < (intoHereMax - 1) ) {
 				
-				CFRange		allChars;
+				CFRange allChars;
 				allChars.location = 0;
 				allChars.length = osxStrLen;
 				
-				CFStringGetCharacters(osxStr, allChars, (UniChar*)(intoHere));
+				CFStringGetCharacters (osxStr, allChars, (UniChar*)(intoHere));
 				intoHere[osxStrLen] = 0x0000;
 				returnResult = (unsigned long)osxStrLen;
 				
@@ -48,13 +48,13 @@ const unsigned long Sub_OSXLoadString ( unsigned long stringID, FMX_Unichar* int
 			
 		} // osxStr
 		
-		if ( osxStr ) { CFRelease( osxStr ); }
-		CFRelease( strIdStr );
-		CFRelease( bundleIdStr );
+		if ( osxStr ) { CFRelease ( osxStr ); }
+		if ( strIdStr ) { CFRelease ( strIdStr ); }
+		if ( bundleIdStr ) { CFRelease ( bundleIdStr ); }
 		
 	} // intoHere
 	
-	return(returnResult);
+	return returnResult;
 	
 } // Sub_OSXLoadString
 
