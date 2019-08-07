fcdb8970be24c19f4eb21e8246afd44d16d81870
Mark Banks <mark@banks.id.au>
2019-08-07 18:20:16 +1000

    Version 4.1.2

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -20,9 +20,9 @@
 // this is the only place the version information is set
 // building the macOS version will automatically set the version information for win/linux
 
-#define	AUTO_UPDATE_VERSION "04010002"
-#define VERSION_NUMBER_STRING "4.1.1" // e.g. 4.0.1
-#define VERSION_STRING "4.1.1"	// e.g. 4.0.1b2
+#define	AUTO_UPDATE_VERSION "04010200"
+#define VERSION_NUMBER_STRING "4.1.2" // e.g. 4.0.1
+#define VERSION_STRING "4.1.2"	// e.g. 4.0.1b2
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
 
