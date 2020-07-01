23718775f461da4adac626859fee8533676f0ec8
Nicholas Orr <nick@goya.com.au>
2020-07-01 20:50:08 +1000

    Update BECppUtilities.cpp
    Copyright change

--- Source/BECppUtilities.cpp
+++ Source/BECppUtilities.cpp
@@ -2,7 +2,7 @@
  BECppUtilities.cpp
  BaseElements Plug-In
  
- Copyright 2013-2015 Goya. All rights reserved.
+ Copyright 2013-2020 Goya. All rights reserved.
  For conditions of distribution and use please see the copyright notice in BEPlugin.cpp
  
  http://www.goya.com.au/baseelements/plugin
