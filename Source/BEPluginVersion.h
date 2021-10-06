49a1461b54d143ea704cd4d1b9fec9981b1c663c
Mark Banks <mark@banks.id.au>
2021-10-07 10:26:25 +1100

    Work around MS's interpretation of MIME::Content-Type::name - BE_SMTPSend [Jira #93]

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -20,9 +20,9 @@
 // this is the only place the version information is set
 // building the macOS version will automatically set the version information for win/linux
 
-#define	AUTO_UPDATE_VERSION "04020104"
-#define VERSION_NUMBER_STRING "4.2.1" // e.g. 4.0.1
-#define VERSION_STRING "4.2.1"	// e.g. 4.0.1b2
+#define	AUTO_UPDATE_VERSION "04020105"
+#define VERSION_NUMBER_STRING "4.2.2" // e.g. 4.0.1
+#define VERSION_STRING "4.2.2b1"	// e.g. 4.0.1b2
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
 
