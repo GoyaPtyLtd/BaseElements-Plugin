62eb60b21e7b1292a7d7a9e8e2a9b47c784a76f0
Mark Banks <mark@banks.id.au>
2023-05-16 09:39:25 +1000

    Update copyright

--- Source/Net/BECurl.cpp
+++ Source/Net/BECurl.cpp
@@ -2,7 +2,7 @@
  BECurl.cpp
  BaseElements Plug-In
 
- Copyright 2011-2022 Goya. All rights reserved.
+ Copyright 2011-2023 Goya. All rights reserved.
  For conditions of distribution and use please see the copyright notice in BEPlugin.cpp
 
  http://www.goya.com.au/baseelements/plugin
