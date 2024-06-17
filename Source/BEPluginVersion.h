d18a70bde59bd5d7201c4b312907b2bb40215ed9
Nicholas Orr <nick@goya.com.au>
2024-06-17 20:26:28 +1000

    Cleanup and add this as 5.1.0

--- Source/BEPluginVersion.h
+++ Source/BEPluginVersion.h
@@ -1,13 +1,9 @@
 /*
  BEPluginVersion.h
  BaseElements Plug-In
-<<<<<<< HEAD
 
- Copyright 2015-2023 Goya. All rights reserved.
-=======
+ Copyright 2015-2024 Goya. All rights reserved.
  
- Copyright 2015-2022 Goya. All rights reserved.
->>>>>>> origin/master
  For conditions of distribution and use please see the copyright notice in BEPlugin.cpp
 
  http://www.goya.com.au/baseelements/plugin
@@ -25,18 +21,10 @@
 // this is the only place the version information is set
 // building the macOS version will automatically set the version information for win/linux
 
-<<<<<<< HEAD
-#define	AUTO_UPDATE_VERSION "05000002"
-#define VERSION_NUMBER_STRING "5.0.0" // e.g. 4.0.1
-#define VERSION_STRING "5.0.0"	// e.g. 4.0.1b2
-
-=======
-#define	AUTO_UPDATE_VERSION "04020600"
-#define VERSION_NUMBER_STRING "4.2.6" // e.g. 4.0.1
-#define VERSION_STRING "4.2.6"	// e.g. 4.0.1b2
->>>>>>> origin/master
+#define	AUTO_UPDATE_VERSION "05010001"
+#define VERSION_NUMBER_STRING "5.1.0" // e.g. 4.0.1
+#define VERSION_STRING "5.1.0"	// e.g. 4.0.1b2
 
 #define USER_AGENT_STRING "libcurl-agent-base-elements-plugin/" VERSION_STRING
-
 
 #endif /* BEPLUGINVERSION_H */
