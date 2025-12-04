fdfa77e5159f5f62442c40c971b6a0eb98bd6d3f
Nicholas Orr <nick@goya.com.au>
2025-12-04 17:34:11 +1100

    Update the image related libraries to latest from the podofo_1.0.2m library repo tag.

--- Headers/libheif/heif_version.h
+++ Headers/libheif/heif_version.h
@@ -28,10 +28,10 @@
 #define LIBHEIF_HEIF_VERSION_H
 
 /* Numeric representation of the version */
-#define LIBHEIF_NUMERIC_VERSION ((1<<24) | (17<<16) | (6<<8) | 0)
+#define LIBHEIF_NUMERIC_VERSION ((1<<24) | (20<<16) | (2<<8) | 0)
 
 /* Version string */
-#define LIBHEIF_VERSION "1.17.6"
+#define LIBHEIF_VERSION "1.20.2"
 
 #define LIBHEIF_PLUGIN_DIRECTORY "/Users/runner/work/BaseElements-Plugin-Libraries/BaseElements-Plugin-Libraries/output/platforms/macos-arm64_x86_64/src/libheif/_build/lib/libheif"
 
